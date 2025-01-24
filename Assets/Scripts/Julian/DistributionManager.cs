311543ddfbd0f8ab31afb76b24c4e74823abf7fc
Jumi <serve.julian.@gmail.com>
2025-01-25 00:55:37 +0100

    Finished Distribution Manager
    s. title

--- Assets/Scripts/Julian/DistributionManager.cs
+++ Assets/Scripts/Julian/DistributionManager.cs
@@ -1,18 +1,175 @@
 using UnityEngine;
+using System.Collections.Generic;
 
 public class DistributionManager : MonoBehaviour
 {
+    public static DistributionManager Instance { get; private set; }
 
+    [SerializeField] private List<SpawnPoint> coinSpawnPoints;
+    [SerializeField] private List<SpawnPoint> vendingMachineSpawnPoints;
+    [SerializeField] private List<SpawnPoint> gumPlaceSpawnPoints;
 
-    // Start is called before the first frame update
-    void Start()
+    private GameObject coinPrefab;
+    private GameObject vendingMachinePrefab;
+    private GameObject gumPlacePrefab;
+
+    [SerializeField] private int minimumGumSpawnPoints = 10;
+    [SerializeField] private int minimumCoins = 3;
+    [SerializeField] private int minimumVendingMachines = 1;
+
+    private int existingGumSpawnPoints;
+    private int existingCoins;
+    private int existingVendingMachines;
+
+    private void Awake()
     {
-        
+        if(Instance == null) Instance = this;
+        else { Destroy(Instance); return; }
+
+        FindObjectsOfType<SpawnPoint>();
+
+        coinPrefab = Resources.Load<GameObject>("Coin");
+        gumPlacePrefab = Resources.Load<GameObject>("GumPlace");
+        vendingMachinePrefab = Resources.Load<GameObject>("VendingMachine");
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Start() => CollectSpawnPoints();
+
+    private void Update()
     {
-        
+        if(existingGumSpawnPoints < minimumGumSpawnPoints)
+        {
+            DistributeGumPlace();
+        }
+
+        if(existingCoins < minimumCoins)
+        {
+            DistributeCoin();
+        }
+
+        if(existingVendingMachines < minimumVendingMachines)
+        {
+            DistributeVendingMachine();
+        }
+    }
+
+    private void CollectSpawnPoints()
+    {
+        SpawnPoint[] spawnPoints = FindObjectsOfType<SpawnPoint>();
+
+        for (int i = 0; i < spawnPoints.Length; i++) 
+        {
+            switch(spawnPoints[i].SpawnPointObject)
+            {
+                case SpawnPointObject.GumPlacePosition:
+                    gumPlaceSpawnPoints.Add(spawnPoints[i]);
+                    break;
+                case SpawnPointObject.Coin:
+                    coinSpawnPoints.Add(spawnPoints[i]);
+                    break;
+                case SpawnPointObject.VendingMachine:
+                    vendingMachineSpawnPoints.Add(spawnPoints[i]);
+                    break;
+            }
+        }
+    }
+
+    #region Gum Place Spawning
+    private void DistributeGumPlace()
+    {
+        bool foundValidSpawnPoint = false;
+        int randomSpawnPointIndex;
+
+        while (!foundValidSpawnPoint) 
+        {
+            randomSpawnPointIndex = Random.Range(0, gumPlaceSpawnPoints.Count);
+
+            SpawnPoint spawnPoint = gumPlaceSpawnPoints[randomSpawnPointIndex];
+
+            if (spawnPoint.HasObject) continue;
+
+            SpawnGumPlace(spawnPoint); return;
+        }
+    }
+
+    private void SpawnGumPlace(SpawnPoint at)
+    {
+        Instantiate(gumPlacePrefab, at.transform.position, Quaternion.identity);
+
+        existingGumSpawnPoints++;
+    }
+    #endregion
+
+    #region Coin Spawning
+    private void DistributeCoin()
+    {
+        bool foundValidSpawnPoint = false;
+        int randomSpawnPointIndex;
+
+        while (!foundValidSpawnPoint)
+        {
+            randomSpawnPointIndex = Random.Range(0, coinSpawnPoints.Count);
+
+            SpawnPoint spawnPoint = coinSpawnPoints[randomSpawnPointIndex];
+
+            if (spawnPoint.HasObject) continue;
+
+            SpawnCoin(spawnPoint); return;
+        }
+    }
+
+    private void SpawnCoin(SpawnPoint at)
+    {
+        Instantiate(coinPrefab, at.transform.position, Quaternion.identity);
+
+        existingCoins++;
+    }
+    #endregion
+
+    #region Vending Machine Spawning
+    private void DistributeVendingMachine() 
+    {
+        bool foundValidSpawnPoint = false;
+        int randomSpawnPointIndex = 0;
+
+        while (!foundValidSpawnPoint)
+        {
+            randomSpawnPointIndex = Random.Range(0, vendingMachineSpawnPoints.Count);
+
+            SpawnPoint spawnPoint = vendingMachineSpawnPoints[randomSpawnPointIndex];
+
+            if (spawnPoint.HasObject) continue;
+
+            SpawnVendingMachine(spawnPoint); return;
+        }
+    }
+
+    private void SpawnVendingMachine(SpawnPoint at)
+    {
+        Instantiate(vendingMachinePrefab, at.transform.position, Quaternion.identity);
+
+        existingVendingMachines++;
+    }
+    #endregion
+
+    public void RemoveSpawnPointObject(SpawnPoint spawnPoint)
+    {
+        spawnPoint.HasObject = false;
+
+        switch (spawnPoint.SpawnPointObject)
+        {
+            case SpawnPointObject.GumPlacePosition:
+                gumPlaceSpawnPoints.Remove(spawnPoint);
+                existingGumSpawnPoints--;
+                break;
+            case SpawnPointObject.Coin:
+                coinSpawnPoints.Remove(spawnPoint);
+                existingCoins--;
+                break;
+            case SpawnPointObject.VendingMachine:
+                vendingMachineSpawnPoints.Remove(spawnPoint);
+                existingVendingMachines--;
+                break;
+        }
     }
 }
---
+++ Assets/Scripts/Julian/SpawnPoint.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public enum SpawnPointObject
+{
+    VendingMachine,
+    Coin,
+    GumPlacePosition
+}
+
+public class SpawnPoint : MonoBehaviour
+{
+    [field: SerializeField] public SpawnPointObject SpawnPointObject { get; private set; }
+
+    [field: SerializeField] public bool HasObject { get; set; } //Checks whether or not an object is at the spawn point -> don't spawn other object
+}
