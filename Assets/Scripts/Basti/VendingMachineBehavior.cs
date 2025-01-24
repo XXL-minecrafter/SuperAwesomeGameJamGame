6d6fee9e0c8ab48c4bb6af2c15986f8fa89d2e2b
NoJokeZ <pfallerbastian@gmail.com>
2025-01-25 00:57:00 +0100

    VendingMachine script updated

--- Assets/Scripts/Basti/VendingMachineBehavior.cs
+++ Assets/Scripts/Basti/VendingMachineBehavior.cs
@@ -1,20 +1,61 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-public class VendingMachineBehavior : MonoBehaviour
+public class VendingMachineBehavior : MonoBehaviour, IInteractable
 {
     private int gumAmount = 2;
 
-    //private PlayerInput playerInput;
+    private PlayerInput playerInput;
+    private InputAction interact;
 
-    private void OnUse()
+    private bool isUsed;
+
+    private PlayerStats playerStats;
+
+    //private SpawnPoint spawnPoint;
+
+
+    private void OnEnable()
+    {
+        playerInput = new PlayerInput();
+        interact = playerInput.Player.interaction;
+
+        playerStats = GameObject.FindWithTag("Player").GetComponent<PlayerStats>();
+    }
+
+    private void Update()
+    {
+        if (isUsed && interact.WasPressedThisFrame())
+        {
+            
+        }
+    }
+
+
+    public void Interaction()
+    {
+        isUsed = true;
+        //Open UI for VendingMachine 
+
+    }
+
+    private void VendingMachinePulled()
     {
         gumAmount -= 1;
 
-        //Open UI for VendingMachine
+        playerStats.setChewing(true);
 
+        //Close UI for VendingMachine
+    }
 
+    private void CheckGumAmount()
+    {
+        if(gumAmount == 0)
+        {
+            
+        }
     }
 }
