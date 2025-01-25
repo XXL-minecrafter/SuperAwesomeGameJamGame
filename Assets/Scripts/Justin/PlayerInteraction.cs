dec368841a23621bb25a077274542b89b5bebbef
Jumi <serve.julian.@gmail.com>
2025-01-25 13:24:27 +0100

    Merge Part One
    s. title

--- Assets/Scripts/Justin/PlayerInteraction.cs
+++ Assets/Scripts/Justin/PlayerInteraction.cs
@@ -1,48 +1,26 @@
-<<<<<<< Updated upstream
-using System;
-=======
->>>>>>> Stashed changes
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class InteractionRadius : MonoBehaviour
 {
-<<<<<<< Updated upstream
-    
-=======
->>>>>>> Stashed changes
     private PlayerInput playerInput;
     private InputAction inputActionInteraction;
-    private InputAction bubbleGumBlast;
-
     private IInteractable interactableObject;
 
-
-
     private float timeStampPressedInteraction;
-    private float interactionHoldTimer;
+    [SerializeField] private float interactionHoldTimer;
     private bool holdingInput = false;
     [SerializeField] private float interactionTime = 5f;
 
-    private bool interactionPressed;
-    private bool interactionFound;
+    private bool interactionPressed = false;
+    [SerializeField] private bool interactionFound;
     private bool isTable;
-
-    public event Action BubbleGumBlast;
 
     private void Awake()
     {
         playerInput = new PlayerInput();
         inputActionInteraction = playerInput.Player.interaction;
-        bubbleGumBlast = playerInput.Player.bubbleGumBlast;
     }
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        interactionPressed = false;
-    }
-
 
     private void OnEnable()
     {
@@ -58,45 +36,25 @@ public class InteractionRadius : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-<<<<<<< Updated upstream
-        if (bubbleGumBlast.WasPressedThisFrame() && PlayerStats.Instance.IsChewing)
-        {
-            
-        }
-
-        if (isTable)
-=======
         //TODO: Gum can also be placed
 
         if (playerInput.Player.interaction.WasPressedThisFrame()) holdingInput = true;
- 
-        if (playerInput.Player.interaction.WasReleasedThisFrame()) 
->>>>>>> Stashed changes
+
+        if (playerInput.Player.interaction.WasReleasedThisFrame())
         {
             holdingInput = false;
             interactionHoldTimer = 0f;
         }
 
-        if(holdingInput)
+        if (holdingInput)
         {
             interactionHoldTimer += Time.deltaTime;
 
-            if(interactionHoldTimer >= interactionTime && isTable)
+            if (interactionHoldTimer >= interactionTime && isTable)
             {
-<<<<<<< Updated upstream
-                if (interactionHoldCounter >= interactionTime)
-                {
-                    interactableObject.Interact();
-                    timeStampPressedInteraction = 0;
-                    interactionHoldCounter = 0;
-                }
-                interactionHoldCounter += Time.deltaTime;
-                timeStampPressedInteraction = Time.time;
-=======
                 interactableObject.Interact(); holdingInput = false;
->>>>>>> Stashed changes
             }
-            else if(!isTable)
+            else if (!isTable)
             {
                 interactableObject.Interact(); holdingInput = false;
             }
@@ -109,20 +67,10 @@ public class InteractionRadius : MonoBehaviour
         {
             interactionFound = true;
             interactableObject = interactable;
-<<<<<<< Updated upstream
-            if (collision.TryGetComponent(out Table table))
-            {
-                Debug.Log("Is Table");
-                isTable = true;
-            }
-            else
-            {
-                isTable = false;
-            }
-=======
-            
-            isTable = interactable is Table;         
->>>>>>> Stashed changes
+
+            interactableObject.ShowInteractionBox();
+
+            isTable = interactable is Table;
         }
     }
     private void OnTriggerExit2D(Collider2D collision)
@@ -130,6 +78,9 @@ public class InteractionRadius : MonoBehaviour
         if (collision.gameObject.TryGetComponent(out IInteractable interactable))
         {
             interactionFound = false;
+
+            interactableObject.DisableInteractionBox();
+
             isTable = false;
         }
     }
--- Assets/Scripts/Justin/Table.cs
+++ Assets/Scripts/Justin/Table.cs
@@ -1,27 +1,18 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Table : MonoBehaviour, IInteractable
 {
-
-    private PlayerStats playerStats;
-
     [field: SerializeField] public GameObject InteractionBox { get ; set ; }
-
-    public void DisableInteractionBox()
-    {
-        throw new System.NotImplementedException();
-    }
-
-    public void Interact()
-    {
-        PlayerStats.Instance.PlacedGum();
-        Debug.Log("Table Interaction Holded");
-    }
 
     public void ShowInteractionBox()
     {
-        throw new System.NotImplementedException();
+        InteractionBox?.SetActive(true);
     }
+    public void DisableInteractionBox()
+    {
+        InteractionBox?.SetActive(false);
+    }
+
+    public void Interact() => PlayerStats.Instance.PlacedGum();
+
 }
