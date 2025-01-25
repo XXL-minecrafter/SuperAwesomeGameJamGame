3951ae6e1b78f489a827564a439e178b7e9aecef
Jumi <serve.julian.@gmail.com>
2025-01-25 01:29:13 +0100

    SpawnPoint behaviour added
    s. title

--- Assets/Scripts/Julian/SpawnPoint.cs
+++ Assets/Scripts/Julian/SpawnPoint.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 
 public enum SpawnPointObject
@@ -12,4 +13,10 @@ public class SpawnPoint : MonoBehaviour
     [field: SerializeField] public SpawnPointObject SpawnPointObject { get; private set; }
 
     [field: SerializeField] public bool HasObject { get; set; } //Checks whether or not an object is at the spawn point -> don't spawn other object
+
+    private static Action<SpawnPoint> onObjectCleared;
+
+    public static event Action<SpawnPoint> OnObjectCleared { add =>  onObjectCleared += value; remove => onObjectCleared -= value; }
+
+    public void RemoveSpawnPointObject() => onObjectCleared?.Invoke(this);
 }
