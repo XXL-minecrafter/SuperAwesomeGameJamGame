e82f48ab385c57342e206b49d477df26f75b39c7
JanBoehling <122728039+JanBoehling@users.noreply.github.com>
2025-01-25 03:16:02 +0100

    Began adding Enemy AI System

---
+++ Assets/Scripts/EnemyAI/EnemyController.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    private void Update()
+    {
+        
+    }
+
+    private bool IsSeeingThings()
+    {
+        return default;
+    }
+}
