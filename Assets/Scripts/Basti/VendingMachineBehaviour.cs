9a93f3ba55e9f93e5ff8c301009b52c372f39fbc
Sell1412 <tobias.hoeflmeier@web.de>
2025-01-26 16:17:36 +0100

    Vending Machine, Ui Updated

--- Assets/Scripts/Basti/VendingMachineBehaviour.cs
+++ Assets/Scripts/Basti/VendingMachineBehaviour.cs
@@ -12,25 +12,35 @@ public class VendingMachineBehaviour : MonoBehaviour, IInteractable
 
     private PlayerStats playerStats;
     public SpawnPoint SpawnPoint;
+    private SpriteRenderer spriteRenderer;
 
     [field: SerializeField] public GameObject InteractionBox { get ; set ; }
+    [SerializeField] Sprite leverPulled;
+    Sprite oldSprite;
+
     private Shaking shake;
     public static Action OnInteract;
 
     private void OnEnable()
     {
+        spriteRenderer = GetComponentInChildren<SpriteRenderer>();
         playerInput = new PlayerInput();
         interact = playerInput.Player.interaction;
         shake =GetComponent<Shaking>();
         playerStats = GameObject.FindWithTag("Player").GetComponent<PlayerStats>();
+        
     }
-
+    private void Start()
+    {
+        oldSprite = spriteRenderer.sprite;
+    }
 
     public void Interact()
     {
         //Play Animation "Lever pulled"
         if (!PlayerStats.Instance.IsChewing)
         {
+            spriteRenderer.sprite = leverPulled;
             OnInteract?.Invoke();
             gumAmount -= 1;
             playerStats.SetChewing(true);
@@ -42,6 +52,7 @@ public class VendingMachineBehaviour : MonoBehaviour, IInteractable
     public IEnumerator CheckGumAmount()
     {
         yield return new WaitForSeconds(shake.setShakeDuration);
+        spriteRenderer.sprite = oldSprite;
         if (gumAmount == 0)
         {
             SpawnPoint.RemoveSpawnPointObject();
